523f688cb048394cf4b8d76659a036963bde38f0
MJF07 <maxfrankel@stu.postoakschool.org>
2024-09-27 13:49:30 -0500

    commit

--- 12345.py
+++ 12345.py
@@ -9,29 +9,60 @@ for i in range (0,10):
 import tkinter as tk
 from tkinter import*
 
-root = tk.Tk()
-root.title("calc")
+#this is globally declared expression
+expression=""
 
-calc_box=Entry(root)
+def press(num):
+    #grab global value of expression
+    global expression
+    expression=expression+str(num)
+    equation.set(expression)
+
+def equal_press():
+    global expression
+    total=str(eval(expression))
+    equation.set(total)
+    expression=""
+
+def clear():
+    global expression
+    expression=""
+    equation.set("")
+
+
+#this is the beginning of TK inter window definitions
+root = tk.Tk()
+
+#setting size and name of window
+root.title("calc")
+root.geometry("250x250")
+
+#setting some variables
+equation= StringVar()
+#entry box that shows text
+calc_box=Entry(root,textvariable=equation)
 calc_box.grid(row=0, column=1)
-#create 3 buttons
-button1 = tk.Button(root,text="7")
-button2 = tk.Button(root,text="8")
-button3 = tk.Button(root,text="9")
-button4 = tk.Button(root,text="6")
-button5 = tk.Button(root,text="5")
-button6 = tk.Button(root,text="4")
-button7 = tk.Button(root,text="3")
-button8 = tk.Button(root,text="2")
-button9 = tk.Button(root,text="1")
-button0 = tk.Button(root,text="0")
-buttonD = tk.Button(root,text="/")
-buttonM = tk.Button(root,text="x")
-buttonS = tk.Button(root,text="-")
-buttonA = tk.Button(root,text="+")
-buttonP = tk.Button(root,text=".")
-buttonE = tk.Button(root,text="Enter")
-buttonC = tk.Button(root,text="Clear")
+
+
+
+#create buttons
+button7 = tk.Button(root,text="7",fg='black' , bg='yellow' , command=lambda: press(7) )
+button8 = tk.Button(root,text="8",fg='black' , bg='yellow' , command=lambda: press(8) )
+button9 = tk.Button(root,text="9",fg='black' , bg='yellow' , command=lambda: press(9) )
+button6 = tk.Button(root,text="6",fg='black' , bg='yellow' , command=lambda: press(6) )
+button5 = tk.Button(root,text="5",fg='black' , bg='yellow' , command=lambda: press(5) )
+button4 = tk.Button(root,text="4",fg='black' , bg='yellow' , command=lambda: press(4) )
+button3 = tk.Button(root,text="3",fg='black' , bg='yellow' , command=lambda: press(3) )
+button2 = tk.Button(root,text="2",fg='black' , bg='yellow' , command=lambda: press(2) )
+button1 = tk.Button(root,text="1",fg='black' , bg='yellow' , command=lambda: press(1) )
+button0 = tk.Button(root,text="0",fg='black' , bg='yellow' , command=lambda: press(0) )
+buttonD = tk.Button(root,text="/",fg='black' , bg='yellow' , command=lambda: press("/"))
+buttonM = tk.Button(root,text="x",fg='black' , bg='yellow' , command=lambda: press("*"))
+buttonS = tk.Button(root,text="-",fg='black' , bg='yellow' , command=lambda: press("-"))
+buttonA = tk.Button(root,text="+",fg='black' , bg='yellow' , command=lambda: press("+"))
+buttonP = tk.Button(root,text=".",fg='black' , bg='yellow' , command=lambda: press("."))
+buttonE = tk.Button(root,text="Enter",command=equal_press,fg="black", bg='green')
+buttonC = tk.Button(root,text="Clear",command=clear, fg='black' , bg='blue')
 buttonB = tk.Button(root,text="<-")
 
 
